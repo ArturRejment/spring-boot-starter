b664e9b4e381065e17e36c3e64d16f4197e10c4e
Artur Rejment <arturrejment1@gmail.com>
2022-03-12 16:52:54 +0100

    feat: create Student class and GET endpoint

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,38 @@
 package com.example.demo;
 
+import com.example.demo.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
+@RestController // This annotation makes class a RESTFull, to serve REST endpoints
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@GetMapping
+	public List<Student> hello(){
+		List<Student> listOfStudents = new ArrayList<>();
+
+		Student student1 = new Student(
+				1L,
+				"Artur",
+				LocalDate.of(2000, Month.NOVEMBER, 5),
+				21,
+				"artur@gmail.com"
+		);
+
+		listOfStudents.add(student1);
+		return listOfStudents;
 	}
 
 }
